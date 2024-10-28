0069edad9beabe867c329b44b226b6132825d90f
TarekEsmail18 <136824760+TarekEsmail18@users.noreply.github.com>
2024-10-28 21:56:56 +0300

    Time: 0 ms (100%), Space: 42.7 MB (5.79%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DeleteDuplicates(ListNode head) {
+        if(head == null) 
+            return null;
+        ListNode temp = head;
+        while(temp.next != null)
+        {
+            if(temp.next.val == temp.val)
+            {
+                temp.next = temp.next.next;
+            }
+            else
+            {
+                temp = temp.next;
+            }
+
+        }
+        
+        
+        return head;
+    }
+}
